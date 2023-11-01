fbc9dffe0144029690e11713ebf8b88cf80058e6
Kunalsinh Solanki <99866867+Kunalsinh@users.noreply.github.com>
2023-11-02 01:01:57 +0530

    Delete T3_Script51.php

--- T3_Script51.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-     //Question 51 array_push() on associative array
-     echo "</br>";
-     $vehicle = array("Bike" => "Harley Davidson","Car" => "Endeavor","Truck" => "TATA");
-     array_push($vehicle,"Herculas");
-     print_r($vehicle);
-
-?>
