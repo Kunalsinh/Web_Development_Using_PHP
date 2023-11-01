57d40809b7864b5256dd44cddf11d4794acaa265
Kunalsinh Solanki <99866867+Kunalsinh@users.noreply.github.com>
2023-11-02 01:02:05 +0530

    Delete T3_Script49.php

--- T3_Script49.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //Question 49 use array_chunk() in array 
-    echo "</br>";
-    $bikes = array("Honda","Royal Enfeild","Hayabusa","Hero","BMW");
-    print_r(array_chunk($bikes,3));
-
-?>
