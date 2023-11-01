9167f0fac9c31cd225345dafb2c74da74f3f61fe
Kunalsinh Solanki <99866867+Kunalsinh@users.noreply.github.com>
2023-11-02 00:56:08 +0530

    Delete Q15_Array.php

--- Q15_Array.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Question 15 Multidimesional Array of Movie details like given below
-    $movies = array(
-        array("Title"=>"The Kashmir Files","Director"=>"vivek Agnihotri","Release Year"=>"2022"),
-        array("Title"=>"The Kerala Story","Director"=>"Sudipto Sen","Release Year"=>"2023")
-    );
-
-    print_r($movies[1]);
-?>
