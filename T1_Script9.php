3a2ae906468412e28600eddb4ba9f947e96c4284
Kunalsinh Solanki <99866867+Kunalsinh@users.noreply.github.com>
2023-11-02 00:58:58 +0530

    Delete T1_Script9.php

--- T1_Script9.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Display the date, month , year using echo statement with a format of DD/MM/YYYY. 
-    $date = 17;
-    $month = 01;
-    $year = 2003;
-
-    echo "Birthdate is $date/$month/$year.";
-?>
