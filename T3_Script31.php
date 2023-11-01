41ad5a33a19b993c3f88ebd86115e66c5427dd4d
Kunalsinh Solanki <99866867+Kunalsinh@users.noreply.github.com>
2023-11-02 01:01:27 +0530

    Delete T3_Script31.php

--- T3_Script31.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-//Question 31 search the specific element in array using array_search()
-$arrFruits = array("Apple","Mango","Banana","Orange","Watermelon","Pineapple");
-echo "</br>"."Apple element on index ".array_search('Apple',$arrFruits);
-?>
